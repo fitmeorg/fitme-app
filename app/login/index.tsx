bfeda42be7e758d1ac69c8a4861ea5869219840b
Sirakusa <herrera.clip@gmail.com>
2024-08-05 00:34:59 -0600

    refactor: login and register

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,31 +1,19 @@
-import React, { createContext } from "react";
-import { View, Text, Pressable, TextInput, StyleSheet } from "react-native";
-import { Link, router } from "expo-router";
-
-let access_token;
-
-const handleSubmit = (mail: string, password: string) => {
-  fetch("http://localhost:3000/auth/login", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      mail,
-      password,
-    }),
-  })
-    .then((response) => response.json())
-    .then((token) => {
-      access_token = token;
-    })
-    .catch((error) => console.error(error));
-};
+import React from "react";
+import { View, Text, Pressable, TextInput } from "react-native";
+import { Link } from "expo-router";
+import { styles } from "./style";
+import { stylePassword } from "./style";
+import { handleSubmit } from "@/components/fetch/post";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Login = () => {
   const [password, onChangePassword] = React.useState("");
   const [mail, onChangeMail] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <View style={styles.container}>
@@ -40,18 +28,26 @@ const Login = () => {
         inputMode="email"
       />
 
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangePassword}
-        value={password}
-        placeholder="password"
-        secureTextEntry
-      />
+      <View style={stylePassword.container}>
+        <TextInput
+          style={stylePassword.input}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="password"
+          secureTextEntry={!showPassword}
+        />
+        <MaterialCommunityIcons
+          name={showPassword ? "eye-off" : "eye"}
+          size={24}
+          color="#aaa"
+          onPress={toggleShowPassword}
+        />
+      </View>
 
       <Link href={"/home"} asChild>
         <Pressable
           style={styles.button}
-          onPress={() => handleSubmit(mail, password)}>
+          onPress={() => handleSubmit("auth/login", { mail, password })}>
           <Text style={styles.buttonText}>Login</Text>
         </Pressable>
       </Link>
@@ -63,42 +59,4 @@ const Login = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 24,
-  },
-  input: {
-    width: "100%",
-    height: 40,
-    marginVertical: 10,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 8,
-    padding: 10,
-  },
-  button: {
-    width: "100%",
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 20,
-    marginTop: 20,
-    borderRadius: 8,
-    backgroundColor: "black",
-  },
-  buttonText: {
-    fontSize: 16,
-    color: "white",
-    fontWeight: "bold",
-  },
-});
-
 export default Login;
-export { access_token };
