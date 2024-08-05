8f5e8cf80549e82c4397ef4a9e86386c23c944e1
Sirakusa <herrera.clip@gmail.com>
2024-08-04 20:04:04 -0600

    feat: add pages with routes

---
+++ app/home/index.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}></View>
+  );
+};
+
+export default Home;
